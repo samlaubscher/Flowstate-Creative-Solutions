66c3b5cf842f59d773a6b3cfcce92554d1b0fc6b
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2021-01-24 22:31:27 +0000

    Add homepage header and css, add basic homepage tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,15 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import index
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+
+    def test_home_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_index_view(self):
+        view = resolve('/')
+        self.assertAlmostEquals(view.func, index)
