efd7bcddbeebd570dec69f74d36fa69aedfe6ce3
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2021-02-01 23:44:19 +0000

    Update shopping cart template

--- cart/contexts.py
+++ cart/contexts.py
@@ -8,6 +8,8 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    discount = 0
+    discount_code = settings.DISCOUNT_CODE
     cart = request.session.get('cart', {})
 
     for item_id, quantity in cart.items():
@@ -22,13 +24,17 @@ def cart_contents(request):
     # need to add discount code algorythm here and adjust grand_total
     # if discount_code in request.POST?
 
+    
+
     grand_total = total
 
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
+        'discount': discount,
         'grand_total': grand_total,
+        'discount_code': discount_code,
     }
 
     return context
