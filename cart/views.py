55dcff2a4402ffe08f0f2144dcbe33d7b4cfd25f
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2021-02-03 22:02:17 +0000

    Add toast css styles, cart preview, checkout button and additional message to finish toasts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect, HttpResponse
 from django.contrib import messages
 
 from products.models import Product
@@ -13,7 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a product to the cart """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(1)
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -32,11 +32,14 @@ def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
