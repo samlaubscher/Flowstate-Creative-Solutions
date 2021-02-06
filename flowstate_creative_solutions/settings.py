529bc8fafa1676b0362c9e376d8182a6da2f982a
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2021-02-06 22:04:04 +0000

    Install crispy_forms and update requirements.txt

--- flowstate_creative_solutions/settings.py
+++ flowstate_creative_solutions/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +68,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'flowstate_creative_solutions.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -83,6 +87,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
