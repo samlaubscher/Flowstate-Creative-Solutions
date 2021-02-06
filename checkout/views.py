cf10644fea70d742049f939c362c5c1b388fd7a0
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2021-02-06 21:52:56 +0000

    Create basic view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, reverse
+from django.contrib import messages
 
-# Create your views here.
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart at the moment")
+        return redirect(reverse('products'))
+    
+    template = 'checkout/checkout.html'
+    context = {}
+
+    return render(request, template, context)
