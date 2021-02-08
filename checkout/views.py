b6ab360cfbf53e5ddbba86742998f09c44d137bb
samlaubscher <42819957+samlaubscher@users.noreply.github.com>
2021-02-08 01:31:42 +0000

    Create initial checkout and checkout success views - issue linking order with line item exists

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import redirect, render, reverse
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.contrib import messages
 from django.conf import settings
 
+from .models import Order, OrderLineItem
+from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
@@ -11,19 +13,42 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your cart at the moment")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+        order = Order() # this is not correct 
 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        for item_id, quantity in cart.items():
+            try:
+                product = Product.objects.get(id=item_id)
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    quantity=quantity,
+                )
+                order_line_item.save()
+            except Product.DoesNotExist:
+                messages.error(request, (
+                    "One of the products in your cart was not found in the database. "
+                    "We apologise. Please contact us for assistance via the contact page!"
+                ))
+                order.delete()
+                return redirect(reverse('view_cart'))
+        
+        return redirect(reverse('checkout_success', args=[order.order_number]))
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "There's nothing in your cart at the moment")
+            return redirect(reverse('products'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key missing. \
@@ -33,6 +58,24 @@ def checkout(request):
     context = {
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+    
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
