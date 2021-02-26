0bd4d8fc8da7c3077d987bf48edc067dd25617be
Sam Laubscher <hakaewt@protonmail.com>
2021-02-26 22:17:59 +0000

    Test new debug settings in settings.py

--- flowstate_creative_solutions/settings.py
+++ flowstate_creative_solutions/settings.py
@@ -32,8 +32,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+# If DEVELOPMENT not in env then DEBUG set to False
+if 'DEVELOPMENT' in env:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     'flowstate-creative-solutions.herokuapp.com',
