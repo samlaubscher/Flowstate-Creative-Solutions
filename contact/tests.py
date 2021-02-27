d1c9c55ffb2df2a07e7cad826329b410ab1ede7e
Sam Laubscher <hakaewt@protonmail.com>
2021-02-27 23:44:13 +0000

    Created contact tests

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,15 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import contact
 
-# Create your tests here.
+
+class ContactPageTests(TestCase):
+
+    def test_contact_status_code(self):
+        url = reverse('contact')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_contact_url_resolves_contact_view(self):
+        view = resolve('/contact/')
+        self.assertAlmostEquals(view.func, contact)
