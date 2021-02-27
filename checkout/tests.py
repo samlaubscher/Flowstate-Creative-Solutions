cde049e7893f838058af923fc0fd56c4fd197879
Sam Laubscher <hakaewt@protonmail.com>
2021-02-27 23:41:39 +0000

    Created checkout tests

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,19 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import checkout, checkout_success
 
-# Create your tests here.
+
+class CheckoutPageTests(TestCase):
+
+    def test_checkout_redirect_status_code(self):
+        url = reverse('checkout')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_checkout_url_resolves_checkout_view(self):
+        view = resolve('/checkout/')
+        self.assertAlmostEquals(view.func, checkout)
+
+    def test_checkout_success_url_resolves_checkout_success_view(self):
+        view = resolve('/checkout/checkout_success/123ordernumber')
+        self.assertAlmostEquals(view.func, checkout_success)
