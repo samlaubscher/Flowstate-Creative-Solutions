13315c4af91cd7e3e976eef7bf03b1d62b010f40
Sam Laubscher <hakaewt@protonmail.com>
2021-02-28 09:34:18 +0000

    Created cart tests

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,16 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .views import view_cart
 
-# Create your tests here.
+
+class TestCartPage(TestCase):
+    """ Tests for the cart page """
+
+    def test_view_cart_status_code(self):
+        url = reverse('view_cart')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_cart_url_resolves_view_cart_view(self):
+        view = resolve('/cart/')
+        self.assertAlmostEquals(view.func, view_cart)
